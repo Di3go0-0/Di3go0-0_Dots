c58b57d1ead6baedfb01ba8c6590791116e47c75
di3go0-0 <d.rincon@utp.edu.co>
2024-11-08 19:40:35 -0500

    update a keymap
    Create a keymap Ctrl + k to  save a file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,8 +3,7 @@
 -- Add any additional keymaps here
 
 local keymap = vim.keymap
-local opts = { noremap = true, silent = false }
-local Util = require("lazyvim.util")
+local opts = { noremap = true, silent = true }
 
 local function safe_del(mode, lhs)
   local status, _ = pcall(function()
@@ -23,13 +22,17 @@ local function set_keymaps()
   safe_del("n", "<C-W>j")
   safe_del("n", "<C-h>")
   safe_del("n", "<C-l>")
-
+  safe_del("n", "<C-s>")
+  --
   -- Añadir nuevas keymaps
-  keymap.set("n", "<C-k>", "<C-w>j", opts) --Down
-  keymap.set("n", "<C-i>", "<C-w>k", opts) --Up
-  keymap.set("n", "<C-j>", "<C-w>h", opts) --Left
-  keymap.set("n", "<C-l>", "<C-w>l", opts) --Right
-  -- keymap.set("n", "<C-s>", "", opts)
+  keymap.set("n", "<C-j>", "<C-w>h", opts) -- Left
+  keymap.set("n", "<C-i>", "<C-w>k", opts) -- Up
+  keymap.set("n", "<C-k>", "<C-w>j", opts) -- Down
+  keymap.set("n", "<C-l>", "<C-w>l", opts) -- Right
+
+  -- Configurar atajo de teclado para guardar un archivo con Ctrl + k en modo normal e insert
+  keymap.set("n", "<C-k>", ":w<CR>", opts)
+  keymap.set("i", "<C-k>", "<Esc>:w<CR>a", opts)
 end
 
 -- Llamar a la función para establecer las keymaps
