377dd7648362dfdec89974a8c7ba3e51dcb2a024
di3go0-0 <d.rincon@utp.edu.co>
2024-11-11 23:38:30 -0500

    Csharp support
    Update nvim to suport omnisharp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,6 +49,27 @@ return {
             },
           },
         },
+        omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
+          keys = {
+            {
+              "gd",
+              LazyVim.has("telescope.nvim") and function()
+                require("omnisharp_extended").telescope_lsp_definitions()
+              end or function()
+                require("omnisharp_extended").lsp_definitions()
+              end,
+              desc = "Goto Definition",
+            },
+          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
         gopls = {
           -- capabilities = capabilities,
           settings = {
