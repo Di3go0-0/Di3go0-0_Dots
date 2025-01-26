2b2d483a9df3bcda72e9e64de09bc7eaf2c2e607
di3go0-0 <d.rincon@utp.edu.co>
2025-01-26 03:36:47 -0500

    update nvim config

---
+++ nvim/lua/oldPlugins/lsp.lua
@@ -0,0 +1,130 @@
+-- Disable "No information available" notification on hover
+-- plus define border for hover window
+vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+  config = config
+    or {
+      border = {
+        { "╭", "Comment" },
+        { "─", "Comment" },
+        { "╮", "Comment" },
+        { "│", "Comment" },
+        { "╯", "Comment" },
+        { "─", "Comment" },
+        { "╰", "Comment" },
+        { "│", "Comment" },
+      },
+    }
+  config.focus_id = ctx.method
+  if not (result and result.contents) then
+    return
+  end
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+end
+
+-- Importa las capacidades de autocompletado de `nvim-cmp` en LSP
+-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- Configuración de servidores LSP
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          -- capabilities = capabilities,
+          settings = {
+            typescript = {
+              inlayHints = {
+                enumMemberValues = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                parameterNames = { enabled = false },
+                parameterTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+        },
+        omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
+          keys = {
+            {
+              "gd",
+              LazyVim.has("telescope.nvim") and function()
+                require("omnisharp_extended").telescope_lsp_definitions()
+              end or function()
+                require("omnisharp_extended").lsp_definitions()
+              end,
+              desc = "Goto Definition",
+            },
+          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
+        gopls = {
+          -- capabilities = capabilities,
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
+            },
+          },
+        },
+        cssls = {
+          capabilities = capabilities,
+          settings = {
+            css = { validate = true },
+            less = { validate = true },
+            scss = { validate = true },
+          },
+        },
+        pyright = {
+          capabilities = capabilities,
+          settings = {
+            python = {
+              analysis = {
+                errors = { enabled = true },
+                info = { enabled = true },
+                warnings = { enabled = true },
+              },
+            },
+          },
+        },
+        jsonls = {
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+          end,
+          settings = {
+            json = {
+              format = {
+                enable = true,
+              },
+              validate = { enable = true },
+            },
+          },
+        },
+        -- marksman = {
+        --   capabilities2 = capabilities2,
+        -- },
+      },
+    },
+  },
+}
---
+++ nvim/lua/oldPlugins/nvim-cmp.lua
@@ -0,0 +1,142 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependences = {
+      "hrsh7th/cmp-nvim-lsp",
+      "cmp-cmp.lua",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-git",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-copilot",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "windwp/nvim-autopairs",
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      -- Variable para rastrear el estado de autocompletado
+      local autocomplete_enabled = true
+
+      -- Función para alternar autocompletado
+      local function toggle_autocomplete(enable)
+        autocomplete_enabled = enable
+        cmp.setup({
+          completion = {
+            autocomplete = enable and { cmp.TriggerEvent.TextChanged } or {}, -- Autocompletar solo si está habilitado
+          },
+        })
+      end
+
+      -- Activamos el autocompletado automático inicialmente
+      toggle_autocomplete(true)
+
+      -- Configuración de mapeos
+      opts.mapping = {
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+        -- Mapeo para cerrar el menú de autocompletado y desactivar autocompletado automático
+        ["<C-e>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.close()
+            toggle_autocomplete(false) -- Desactiva el autocompletado
+          else
+            fallback()
+          end
+        end),
+
+        -- Mapeo para abrir el menú de autocompletado y reactivar el autocompletado automático
+        ["<C-Space>"] = cmp.mapping(function()
+          cmp.complete()
+          toggle_autocomplete(true) -- Reactiva el autocompletado automático
+        end),
+      }
+
+      -- Fuentes de autocompletado
+      opts.sources = cmp.config.sources({
+        { name = "copilot", priority = 1000 },
+        { name = "nvim_lsp", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+        { name = "luasnip", priority = 300 },
+        { name = "emmet_nvim" },
+      })
+
+      -- Configuración de ventanas de autocompletado y documentación
+      opts.window = {
+        completion = {
+          border = {
+            { "󱐋", "WarningMsg" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+          scrollbar = false,
+          winblend = 0,
+        },
+        documentation = {
+          border = {
+            { "󰙎", "DiagnosticHint" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+          scrollbar = false,
+          winblend = 0,
+        },
+      }
+    end,
+  },
+}
+
+-- return {
+--   {
+--     "hrsh7th/nvim-cmp",
+--     opts = function(_, opts)
+--       opts.completion.autocomplete = false
+--       opts.mapping["<CR>"] = nil
+--       opts.window = {
+--         completion = {
+--           border = {
+--             { "󱐋", "WarningMsg" },
+--             { "─", "Comment" },
+--             { "╮", "Comment" },
+--             { "│", "Comment" },
+--             { "╯", "Comment" },
+--             { "─", "Comment" },
+--             { "╰", "Comment" },
+--             { "│", "Comment" },
+--           },
+--           scrollbar = false,
+--           winblend = 0,
+--         },
+--         documentation = {
+--           border = {
+--             { "󰙎", "DiagnosticHint" },
+--             { "─", "Comment" },
+--             { "╮", "Comment" },
+--             { "│", "Comment" },
+--             { "╯", "Comment" },
+--             { "─", "Comment" },
+--             { "╰", "Comment" },
+--             { "│", "Comment" },
+--           },
+--           scrollbar = false,
+--           winblend = 0,
+--         },
+--       }
+--     end,
+--   },
+-- }
