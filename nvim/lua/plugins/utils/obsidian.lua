96421ce358b26b16bf733dc05c974bc1ec421951
di3go0-0 <d.rincon@utp.edu.co>
2025-01-26 11:18:31 -0500

    update plugins

--- nvim/lua/plugins/utils/obsidian.lua
+++ nvim/lua/plugins/utils/obsidian.lua
@@ -22,12 +22,12 @@ return {
         },
       },
 
-      -- Completion settings
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
-
+      -- -- Completion settings
+      -- completion = {
+      --   nvim_cmp = true,
+      --   min_chars = 2,
+      -- },
+      --
       notes_subdir = "limbo", -- Subdirectory for notes
       new_notes_location = "limbo", -- Location for new notes
 
