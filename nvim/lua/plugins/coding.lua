414fc63958795bba05b39be0ae73cde259dda9cf
di3go0-0 <d.rincon@utp.edu.co>
2025-01-27 18:15:31 -0500

    update

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,7 +1,7 @@
 return {
 	require("plugins.coding.auto_safe"),
 	require("plugins.coding.avante"),
-	require("plugins.coding.copilot"),
+	-- require("plugins.coding.copilot"),
 	require("plugins.coding.cmp"),
 	require("plugins.coding.lspconfig"),
 	require("plugins.coding.luasnip"),
--- nvim/lua/plugins/coding/cmp.lua
+++ nvim/lua/plugins/coding/cmp.lua
@@ -35,22 +35,19 @@ return {
 
 			-- Configuración de mapeos
 			opts.mapping = {
-				["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<Space>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.close()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				-- Select the [n]ext item
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-
-				-- Scroll the documentation window [b]ack / [f]orward
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
 				-- Mapeo para cerrar el menú de autocompletado y desactivar autocompletado automático
 				["<C-e>"] = cmp.mapping(function(fallback)
@@ -61,6 +58,16 @@ return {
 						fallback()
 					end
 				end),
+
+				-- Scroll the documentation window [b]ack / [f]orward
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+				-- Accept ([y]es) the completion.
+				--  This will auto-import if your LSP supports it.
+				--  This will expand snippets if the LSP sent a snippet.
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
 				-- Mapeo para abrir el menú de autocompletado y reactivar el autocompletado automático
 				["<C-Space>"] = cmp.mapping(function()
 					cmp.complete()
@@ -77,6 +84,22 @@ return {
 				{ name = "luasnip", priority = 300 },
 				{ name = "emmet_nvim" },
 			})
+
+			-- Configuración de íconos para cada tipo de fuente
+			opts.formatting = {
+				format = function(entry, vim_item)
+					local icons = {
+						copilot = "",
+						nvim_lsp = "",
+						buffer = "﬘",
+						path = "",
+						luasnip = "",
+						emmet_nvim = "",
+					}
+					vim_item.kind = string.format("%s %s", icons[entry.source.name], vim_item.kind)
+					return vim_item
+				end,
+			}
 
 			-- Configuración de ventanas de autocompletado y documentación
 			opts.window = {
