21ea3d2ca0c8d0020efdb9f7b2f1a5787aec267e
D.rincon <d.rincon@utp.edu.co>
2025-03-18 08:14:12 -0500

    Update colorsheme
    OldWorld

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -140,6 +140,19 @@ return {
 		end,
 	},
 
+	--------------
+	-- oldworld --
+	--------------
+
+	{
+		"dgox16/oldworld.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			variant = "oled",
+		},
+	},
+
 	--------------------------
 	--  Main Configuration  --
 	--------------------------
