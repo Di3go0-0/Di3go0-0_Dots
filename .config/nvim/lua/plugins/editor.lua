97e52e37033c02aad497b090c8aad95d97d56c35
Di3go0-0 <d.rincon@utp.edu.co>
2025-05-08 17:54:39 -0500

    Update tree

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,6 @@
 return {
 	{ import = "plugins.editor.editor" },
-	{ import = "plugins.editor.neotree" },
+	{ import = "plugins.editor.nvim-tree" },
+	-- { import = "plugins.editor.neotree" },
 	{ import = "plugins.editor.oil-nvim" },
 }
