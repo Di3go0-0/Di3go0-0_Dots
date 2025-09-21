3983ffab7e871fa03226e2355a79331623b1ef73
Di3go0-0 <d.rincon@utp.edu.co>
2025-09-20 19:16:54 -0500

    Feat: update nvim

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -51,28 +51,28 @@ return {
 	----------------
 	-- Catppuccin --
 	----------------
-
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		opts = {
-			transparent_background = true,
-			flavour = "mocha",
-		},
-		integrations = {
-			cmp = true,
-			gitsigns = true,
-			nvimtree = true,
-			treesitter = true,
-			notify = false,
-			mini = {
-				enabled = true,
-				indentscope_color = "",
-			},
-			-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-		},
-	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	lazy = false,
+	-- 	opts = {
+	-- 		transparent_background = true,
+	-- 		flavour = "mocha",
+	-- 	},
+	-- 	integrations = {
+	-- 		cmp = true,
+	-- 		gitsigns = true,
+	-- 		nvimtree = true,
+	-- 		treesitter = true,
+	-- 		notify = false,
+	-- 		mini = {
+	-- 			enabled = true,
+	-- 			indentscope_color = "",
+	-- 		},
+	-- 		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	-- 	},
+	-- },
 
 	--------------
 	-- kanagawa --
@@ -177,7 +177,7 @@ return {
 		opts = {
 			-- change this line to change the color scheme
 			colorscheme = "gentleman-kanagawa-blur",
-			-- colorscheme = "catppuccin-mocha",
+			-- colorscheme = "catppuccin-macchiato",
 			-- colorscheme = "cyberdream",
 			-- colorscheme = "kanagawa-dragon",
 			opts = {
