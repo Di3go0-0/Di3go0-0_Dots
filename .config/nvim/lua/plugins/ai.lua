8e608dd8a4997480fa28ea04d63acf07c0af6612
Di3go0-0 <d.rincon@utp.edu.co>
2025-12-16 21:04:37 -0500

    Feature update modules

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -2,6 +2,6 @@ return {
 	-- { import = "plugins.ai.avante" },
 	{ import = "plugins.ai.copilot-chat" },
 	{ import = "plugins.ai.copilot" },
-	{ import = "plugins.ai.mcphub" },
+	-- { import = "plugins.ai.mcphub" },
 	{ import = "plugins.ai.opencode" },
 }
