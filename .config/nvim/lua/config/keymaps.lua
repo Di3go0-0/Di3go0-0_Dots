a7ea0c890a8f3ca1342c8f344a2518f9784e12b5
Di3go0-0 <d.rincon@utp.edu.co>
2025-12-17 14:10:38 -0500

    Feature update keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 local function set_keymaps()
-	keymap.set("i", "<C-s>", "<Esc>:w<CR>a", opts)
+	keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { desc = "Save file and stay in insert mode" })
 	keymap.set("v", "<leader>sc", ":SSSelected<CR>", { desc = "Capture selected code" })
 	-- Open oil
 	keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
@@ -10,10 +10,10 @@ local function set_keymaps()
 	keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle NeoTree" })
 
 	-- keymap.set("n", "<leader>an", "<cmd>AvanteNewChat<cr>", { desc = "Avante: New Chat" })
-	keymap.set("n", "te", ":tabedit") -- N
-	keymap.set("n", "<tab>", ":tabnext<Return>") -- Próxima tab
-	keymap.set("n", "<s-tab>", ":tabprev<Return>") -- Tab anterior
-	keymap.set("n", "<leader><tab>d", ":tabclose<Return>") -- Cerrar tabueva tab
+	keymap.set("n", "te", ":tabedit", opts) -- New tab
+	keymap.set("n", "<tab>", ":tabnext<Return>", opts) -- Next tab
+	keymap.set("n", "<s-tab>", ":tabprev<Return>", opts) -- Previous tab
+	keymap.set("n", "<leader><tab>d", ":tabclose<Return>", opts) -- Close current tab
 end
 
 set_keymaps()
