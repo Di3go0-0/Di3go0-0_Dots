1c82fccaf700a0b8cc48985ab4043f41b51dc19b
Di3go0-0 <d.rincon@utp.edu.co>
2025-12-17 21:21:04 -0500

    Feature update confirm

--- .config/nvim/lua/plugins/formatting/conform.lua
+++ .config/nvim/lua/plugins/formatting/conform.lua
@@ -1,10 +1,48 @@
+local supported = {
+	"css",
+	"graphql",
+	"handlebars",
+	"html",
+	"javascript",
+	"javascriptreact",
+	"json",
+	"jsonc",
+	"less",
+	"markdown",
+	"markdown.mdx",
+	"scss",
+	"typescript",
+	"typescriptreact",
+	"vue",
+	"yaml",
+}
+
 return {
 	{
 		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
-		},
+		opts = function(_, opts)
+			-- 1. Configuración de formateadores por tipo de archivo
+			opts.formatters_by_ft = opts.formatters_by_ft or {}
+			opts.formatters_by_ft.lua = { "stylua" }
+
+			for _, ft in ipairs(supported) do
+				-- { { "a", "b" } } significa: intenta con 'a', si no está, usa 'b'
+				opts.formatters_by_ft[ft] = { { "prettierd", "prettier" } }
+			end
+
+			-- 2. Configuración de Formateo al Guardar
+			-- Cambiamos format_after_save por format_on_save según tu requerimiento
+			opts.format_on_save = function(bufnr)
+				-- Aquí puedes añadir lógica extra, por ejemplo, para ignorar archivos grandes
+				return {
+					timeout_ms = 500,
+					lsp_fallback = true,
+					quiet = true,
+				}
+			end
+
+			-- Opcional: Si quieres mantener format_after_save en false para evitar conflictos
+			opts.format_after_save = false
+		end,
 	},
 }
