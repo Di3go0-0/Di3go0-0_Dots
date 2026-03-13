60ef2e4f95bdf034a6b5d45d33e2bdd5623fe653
Di3go0-0 <d.rincon@utp.edu.co>
2026-03-12 21:56:16 -0500

    Feature: update config

--- .config/nvim/lua/plugins/languages.lua
+++ .config/nvim/lua/plugins/languages.lua
@@ -1,8 +1,9 @@
 return {
 	-- { import = "plugins.languages.OmniSharp" },
+	{ import = "plugins.languages.docker" },
 	{ import = "plugins.languages.markdown" },
 	{ import = "plugins.languages.mason" },
-	{ import = "plugins.languages.python" },
-	{ import = "plugins.languages.rust" },
+	-- { import = "plugins.languages.python" },
+	-- { import = "plugins.languages.rust" },
 	{ import = "plugins.languages.typescript" },
 }
