b760074fbf366f25ef2c51373377e95e15fa1e6d
Di3go0-0 <d.rincon@utp.edu.co>
2026-04-06 20:13:28 -0500

    Feature update nvim

--- .config/nvim/lua/plugins/editor/restui.lua
+++ .config/nvim/lua/plugins/editor/restui.lua
@@ -1,13 +1,14 @@
 return {
 	{
 		"Di3go0-0/restui.nvim",
+		branch = "main",
 		lazy = true,
 		cmd = { "Restui" },
 		keys = {
 			{ "<leader>rr", "<cmd>Restui<cr>", desc = "Restui HTTP Client" },
 		},
 		config = function()
-			require("restui").setup({
+			require(".config.nvim.lua.plugins.databases.dbtui").setup({
 				float_opts = {
 					width = 0.95,
 					height = 0.95,
